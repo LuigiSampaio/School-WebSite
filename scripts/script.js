a44d440f8113c2958f57bf7e5a0d3b7eda8d82d6
Luigi_Bross <cursosinformatica9102@gmail.com>
2021-09-27 20:42:43 -0300

    finalizando section contact responsivo

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
 
-    // add menu hamburger responsive
+    // adicionando menu hamburger no navbar-menu
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
 
-    // add modal with form login
+    // adicionando modal da pagina de login
     $('#login').click(function () {
         $('.login-form').addClass('popup');
     });
 
-    // remover modal form login 
+    // removendo modal da pagina de login
     $('.login-form form .fa-times').click(function () {
         $('.login-form').removeClass('popup');
     });
 
-    // register scroll 
+    // scroll da pagina 
     $(window).on('load scroll', function () {
 
         $('#menu').removeClass('fa-times');
@@ -24,5 +24,18 @@ $(document).ready(function () {
 
         $('.login-form').removeClass('popup');
 
+        // calculando scroll para ativar classe active no navbar-menu 
+        $('section').each(function () {
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id');
+            let offset = $(this).offset().top - 200;
+
+            // paginacao conforme navbar-menu
+            if (top > offset && top < offset + height) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
     });
 });
